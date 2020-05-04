e089e6d168efab17585833b287a71371f9d25124
Chris Holdgraf <choldgraf@berkeley.edu>
2020-05-04 15:06:25 -0700

    dev bump

--- pydata_sphinx_theme/__init__.py
+++ pydata_sphinx_theme/__init__.py
@@ -8,7 +8,7 @@ from sphinx.errors import ExtensionError
 from .bootstrap_html_translator import BootstrapHTML5Translator
 import docutils
 
-__version__ = "0.2.2"
+__version__ = "0.2.3dev0"
 
 
 def add_toctree_functions(app, pagename, templatename, context, doctree):
