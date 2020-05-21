4b2b528ecce3a7b133f8c682de431b832b6c4908
Chris Holdgraf <choldgraf@berkeley.edu>
2020-05-20 21:05:12 -0700

    adding custom admonitions classes

--- docs/demo/demo.rst
+++ docs/demo/demo.rst
@@ -428,15 +428,30 @@ Admonitions
 
    You can make up your own admonition too.
 
-.. admonition:: If you add a name flag, it will be styled
-   :name: warning
+.. admonition:: If you add a class flag, it will be styled
+   :class: warning
 
    For example, this admonition block uses the following code:
 
    .. code-block::
 
       .. admonition:: If you add a name flag, it will be styled
-         :name: warning
+         :class: warning
+
+   Here are a list of classes you can try:
+
+   .. code-block::
+
+      note
+      important
+      tip
+      attention
+      caution
+      warning
+      danger
+      error
+      hint
+
 
 Topics, Sidebars, and Rubrics
 -----------------------------
@@ -499,7 +514,7 @@ HTML
 The HTML below shouldn't display, but it uses RequireJS to make sure that all
 works as expected. If the widgets don't show up, RequireJS may be broken.
 
-.. jupyter-execute:: 
+.. jupyter-execute::
 
    import plotly.io as pio
    import plotly.express as px
--- pydata_sphinx_theme/bootstrap_html_translator.py
+++ pydata_sphinx_theme/bootstrap_html_translator.py
@@ -2,28 +2,10 @@
 """
 from docutils import nodes
 
-from sphinx.locale import admonitionlabels
 from sphinx.writers.html5 import HTML5Translator
 from sphinx.util import logging
 
 logger = logging.getLogger(__name__)
-
-
-# Mapping of admonition classes to Bootstrap contextual classes
-alert_classes = {
-    "attention": "primary",
-    "caution": "warning",
-    "danger": "danger",
-    "error": "danger",
-    "hint": "info",
-    "important": "primary",
-    "note": "info",
-    "seealso": "info",
-    "tip": "primary",
-    "warning": "warning",
-    "todo": "info",
-    "example": "info",
-}
 
 
 class BootstrapHTML5Translator(HTML5Translator):
@@ -36,47 +18,6 @@ class BootstrapHTML5Translator(HTML5Translator):
     def __init__(self, *args, **kwds):
         super().__init__(*args, **kwds)
         self.settings.table_style = "table"
-
-    def visit_admonition(self, node, name=""):
-        # type: (nodes.Element, str) -> None
-        # copy of sphinx source to add alert classes
-        classes = ["alert"]
-
-        # If we have a generic admonition block, style it as info
-        if (
-            any("admonition-" in iclass for iclass in node.attributes["classes"])
-            and name == ""
-        ):
-            name = "admonition"
-            if node.attributes.get("names"):
-                # If `name` is specified, try to look it up in the list of alerts
-                alert_name = node.attributes.get("names")[0]
-            else:
-                # If no `name` is specified, style it as `note`
-                alert_name = "note"
-            if alert_name not in alert_classes:
-                logger.warning(
-                    f"Unsupported admonition name: `{alert_name}`. Using style `note`.",
-                    location=(self.docnames[0], node.children[0].line),
-                )
-                alert_name = "note"
-
-            # Add the "admonition" class to alert_classes so that it can be referenced
-            alert_classes[name] = alert_classes[alert_name]
-
-            # Remove the title from this admonition and add it to the admonitionlabels
-            # Because this is how Sphinx inserts titles into admonitions
-            title = node.children.pop(0)
-            admonitionlabels[name] = title.astext()
-
-        if name:
-            classes.append("alert-{0}".format(alert_classes[name]))
-
-        # This mimics what Sphinx does in its own `visit_admonition`
-        # but wraps in `alert`
-        self.body.append(self.starttag(node, "div", CLASS=" ".join(classes)))
-        if name:
-            node.insert(0, nodes.title(name, admonitionlabels[name]))
 
     def visit_table(self, node):
         # type: (nodes.Element) -> None
