cdccf546c49da8255fd71750d8af65d59fb81529
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-03-25 09:52:28 +0100

    RLS: v0.5.2

--- pydata_sphinx_theme/__init__.py
+++ pydata_sphinx_theme/__init__.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup as bs
 
 from .bootstrap_html_translator import BootstrapHTML5Translator
 
-__version__ = "0.5.2dev0"
+__version__ = "0.5.2"
 
 
 def add_toctree_functions(app, pagename, templatename, context, doctree):
