055ae27cdc6801d6abba0c43bfa9cfd3a359e1b9
Jack Minchin <35776533+jackminchin@users.noreply.github.com>
2021-09-16 14:45:37 +0100

    FIX: added gtag date and moved google analytics to header (#439)
    Co-authored-by: Jack Minchin <jack@thisisstratum.co.uk>
    Co-authored-by: Chris Holdgraf <choldgraf@gmail.com>
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- pydata_sphinx_theme/__init__.py
+++ pydata_sphinx_theme/__init__.py
@@ -268,6 +268,7 @@ def add_toctree_functions(app, pagename, templatename, context, doctree):
                 <script>
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){{ dataLayer.push(arguments); }}
+                    gtag('js', new Date());
                     gtag('config', '{id}');
                 </script>
                 """
