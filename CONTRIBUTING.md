a67405cb81dd9ccbbb5ef351df5f375c8ceebfaf
Chris Holdgraf <choldgraf@berkeley.edu>
2021-10-03 11:14:53 -0700

    Improve developer instructions

---
+++ CONTRIBUTING.md
@@ -0,0 +1,8 @@
+# Contributing to this theme
+
+Contributions are very welcome! Installing the development version, building
+the demo docs and developing the css/js of the theme, etc, is explained in
+more detail in the contributing section of the documentation:
+
+- [Contributing source files](docs/contributing.md)
+- [Contributing rendered docs](https://pydata-sphinx-theme.readthedocs.io/en/latest/contributing.html)
--- noxfile.py
+++ noxfile.py
@@ -1,30 +1,47 @@
 import nox
+from pathlib import Path
+from yaml import safe_load
 
 nox.options.reuse_existing_virtualenvs = True
 
+# Parse the environment files we'll need later
+environment = safe_load(Path("environment.yml").read_text())
+conda = environment.get("dependencies")
+requirements = conda.pop(-1).get("pip")
 build_command = ["-b", "html", "docs", "docs/_build/html"]
 
 
 @nox.session(venv_backend="conda")
 def build(session):
-    session.conda_install("--channel=conda-forge", "yarn=1.22.15")
-    session.run("yarn")
+    _install_environment(session)
+    session.run("yarn", "--frozen-lockfile")
     session.run("yarn", "build")
 
 
 @nox.session(venv_backend="conda")
 def docs(session):
-    session.install("-r", "docs/requirements.txt")
-    session.install("-e", ".")
-    session.run("sphinx-build", "docs", "docs/_build/html")
+    _install_environment(session)
+    session.run("yarn", "--frozen-lockfile")
+    session.run("make", "html", cwd="docs")
 
 
 @nox.session(name="docs-live", venv_backend="conda")
 def docs_live(session):
-    session.conda_install("--channel=conda-forge", "yarn=1.22.15")
-    session.run("yarn")
-
-    session.install("-r", "docs/requirements.txt")
-    session.install("-e", ".")
-
+    _install_environment(session)
+    session.run("yarn", "--frozen-lockfile")
     session.run("yarn", "build:dev")
+
+
+@nox.session(name="docs-live", venv_backend="conda")
+def tests(session):
+    _install_environment(session)
+    session.run("pytest")
+
+
+def _install_environment(session):
+    for conda_pkg in conda:
+        session.conda_install(conda_pkg)
+    for pkg in requirements:
+        # We split each line in case there's a space for `-r`
+        session.install(*pkg.split())
+    session.install("-e", ".")
