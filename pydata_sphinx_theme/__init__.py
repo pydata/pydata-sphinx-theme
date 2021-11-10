a00a39ddf98e406bbd79aa3eaa630271c5aac595
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-11-10 20:46:46 +0100

    RLS: v0.7.2

--- pydata_sphinx_theme/__init__.py
+++ pydata_sphinx_theme/__init__.py
@@ -13,7 +13,7 @@ from bs4 import BeautifulSoup as bs
 
 from .bootstrap_html_translator import BootstrapHTML5Translator
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 logger = logging.getLogger(__name__)
 
