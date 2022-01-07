c5eb5a0bb5224ab9b7c8b264aa5f7dbad95f114d
Chris Holdgraf <choldgraf@berkeley.edu>
2022-01-06 16:29:59 -0800

    DOCS: refactor dev docs

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,53 @@
 import nox
+from pathlib import Path
 
 nox.options.reuse_existing_virtualenvs = True
 
 
+def _should_install(session):
+    """Decide if we should install an environment or if it already exists.
+
+    This speeds up the local install considerably because building the wheel
+    for this package takes some time.
+
+    We assume that if `sphinx-build` is in the bin/ path, the environment is
+    installed.
+    """
+    bin_files = list(Path(session.bin).glob("*"))
+    sphinx_is_installed = any("sphinx-build" in ii.name for ii in bin_files)
+    force_reinstall = "reinstall" in session.posargs
+    should_install = not sphinx_is_installed or force_reinstall
+    if should_install:
+        session.log("Installing fresh environment...")
+    else:
+        session.log("Skipping environment install...")
+
+
+@nox.session
+def compile(session):
+    if _should_install(session):
+        session.install(".")
+        session.install("sphinx-theme-builder[cli]")
+    session.run("stb", "compile")
+
+
 @nox.session
 def docs(session):
-    session.install(".[doc]")
+    if _should_install(session):
+        session.install(".[doc]")
     session.run("sphinx-build", "-b=html", "docs/", "docs/_build/html")
 
 
 @nox.session(name="docs-live")
 def docs_live(session):
-    session.install(".[doc]")
-    session.install("sphinx-theme-builder[cli]")
+    if _should_install(session):
+        session.install(".[doc]")
+        session.install("sphinx-theme-builder[cli]")
     session.run("stb", "serve", "docs", "--open-browser")
 
 
 @nox.session(name="test")
 def test(session):
-    session.install("-e", ".[test]")
+    if _should_install(session):
+        session.install("-e", ".[test]")
     session.run("pytest", *session.posargs)
