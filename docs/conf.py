a17a4001b80636ff3771168b06a5d97550cc6f82
Chris Holdgraf <choldgraf@berkeley.edu>
2022-01-11 11:46:36 -0800

    FIX: Admonitions margin

--- docs/conf.py
+++ docs/conf.py
@@ -65,6 +65,11 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# -- Extension options -------------------------------------------------------
+
+myst_enable_extensions = [
+    "colon_fence",
+]
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
