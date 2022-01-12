ba72c5e881fc8d5dda2332590ad781221c721e1b
Chris Holdgraf <choldgraf@gmail.com>
2022-01-12 11:31:33 -0800

    Improve the scrolling behavior

--- src/pydata_sphinx_theme/assets/scripts/index.js
+++ src/pydata_sphinx_theme/assets/scripts/index.js
@@ -57,8 +57,9 @@ function scrollToActive() {
         latest_active.getBoundingClientRect().y -
         sidebar.getBoundingClientRect().y;
       // Only scroll the navbar if the active link is lower than 50% of the page
-      if (offset > sidebar.clientHeight * 0.5) {
-        sidebar.scrollTop = offset - sidebar.clientHeight * 0.3;
+      if (latest_active.getBoundingClientRect().y > (window.innerHeight * 0.5)) {
+        let buffer = 0.25;  // Buffer so we have some space above the scrolled item
+        sidebar.scrollTop = offset - sidebar.clientHeight * buffer;
         console.log("[PST]: Scrolled sidebar using last active link...");
       }
     }
