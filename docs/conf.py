e4c68a44e953210faf5c2ca90c9120dc58a4cddd
12rambau <pierrick.rambaud49@gmail.com>
2022-03-09 08:34:06 +0100

    set back the theme-switcher in pydata documentation

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ html_theme_options = {
     # "navbar_align": "left",  # [left, content, right] For testing that the navbar items align properly
     # "navbar_start": ["navbar-logo", "navbar-version"],
     # "navbar_center": ["navbar-nav", "navbar-version"],  # Just for testing
-    "navbar_end": ["version-switcher", "navbar-icon-links"],
+    "navbar_end": ["theme-switcher", "version-switcher", "navbar-icon-links"],
     # "left_sidebar_end": ["custom-template.html", "sidebar-ethical-ads.html"],
     # "footer_items": ["copyright", "sphinx-version", ""]
     "switcher": {
