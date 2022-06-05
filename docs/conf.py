03d8446b58a6b962dc10bf663e5b189f69784fc3
Chris Holdgraf <choldgraf@berkeley.edu>
2022-06-05 07:06:04 +0200

    Fixing logo variables

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,9 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
-
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 
-project = "PyData Sphinx Theme"
+project = "PyData Theme"
 copyright = "2019, PyData Community"
 author = "PyData Community"
 
@@ -26,10 +11,6 @@ import pydata_sphinx_theme
 
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 
 extensions = [
     "jupyter_sphinx",
@@ -80,6 +61,7 @@ myst_enable_extensions = [
 html_theme = "pydata_sphinx_theme"
 html_logo = "_static/logo.svg"
 html_favicon = "_static/logo.svg"
+html_title = "PyData Theme"
 
 # Define the json_url for our version switcher.
 json_url = "https://pydata-sphinx-theme.readthedocs.io/en/latest/_static/switcher.json"
