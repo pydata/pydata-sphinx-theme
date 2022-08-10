5520b9569b2ada468bb7379f039bdf59f4e6afda
Rambaud Pierrick <12rambau@users.noreply.github.com>
2022-08-10 13:48:11 +0200

    style: add line break for readability

--- docs/scripts/gallery_directive/__init__.py
+++ docs/scripts/gallery_directive/__init__.py
@@ -78,15 +78,19 @@ class GalleryDirective(SphinxDirective):
             options = {}
             if "link" in item:
                 options["link"] = item["link"]
-
+                
             if "class-card" in self.options:
                 options["class-card"] = self.options["class-card"]
+                
             if "img-background" in item:
                 options["img-background"] = item["img-background"]
+                
             if "img-top" in item:
                 options["img-top"] = item["img-top"]
+                
             if "img-bottom" in item:
                 options["img-bottom"] = item["img-bottom"]
+                
             options_str = "\n".join(f":{k}: {v}" for k, v in options.items()) + "\n\n"
 
             # Grid card content
