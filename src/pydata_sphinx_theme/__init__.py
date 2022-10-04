e2e6b4c12e25c00e576d1aa1266e5fc5b05bfe41
Jarrod Millman <jarrod.millman@gmail.com>
2022-10-04 13:18:16 -0700

    RLS: v0.11.0rc3

--- src/pydata_sphinx_theme/__init__.py
+++ src/pydata_sphinx_theme/__init__.py
@@ -24,7 +24,7 @@ import requests
 
 from .bootstrap_html_translator import BootstrapHTML5Translator
 
-__version__ = "0.11.0rc3.dev0"
+__version__ = "0.11.0rc3"
 
 logger = logging.getLogger(__name__)
 
