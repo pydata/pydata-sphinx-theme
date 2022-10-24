ccaa0413fc348c464a25e076aa3032e19e6d9892
12rambau <pierrick.rambaud49@gmail.com>
2022-10-24 09:50:00 +0200

    typo

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@
  */
 
 const { resolve } = require("path");
-//const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
