05c2601238e7aed75907107ebeba274072e00c1c
Chris Holdgraf <choldgraf@berkeley.edu>
2023-02-03 19:54:14 +0100

    Bump to dev0

--- src/pydata_sphinx_theme/__init__.py
+++ src/pydata_sphinx_theme/__init__.py
@@ -27,7 +27,7 @@ from requests.exceptions import ConnectionError, HTTPError, RetryError
 
 from .translator import BootstrapHTML5TranslatorMixin
 
-__version__ = "0.13.0rc1"
+__version__ = "0.13.0rc2.dev0"
 
 logger = logging.getLogger(__name__)
 
