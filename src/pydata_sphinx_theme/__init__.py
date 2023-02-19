490d8b9c9a43c4077ed664ada5b23c6717555ff0
Chris Holdgraf <choldgraf@berkeley.edu>
2023-02-19 05:44:07 +0100

    Revert complex config set

--- src/pydata_sphinx_theme/__init__.py
+++ src/pydata_sphinx_theme/__init__.py
@@ -32,19 +32,9 @@ __version__ = "0.13.0rc5dev0"
 logger = logging.getLogger(__name__)
 
 
-def _set_config_if_not_provided_by_user(app, key, value):
-    """Set a configuration value unless the user has provided their own.
-
-    Sphinx makes it hard to know which config attribute to update,
-    so just use this function.
-    """
-    # Check if the user has manually provided the config
-    if any(key in ii for ii in [app.config.overrides, app.config._raw_config]):
-        return
-
-    # If not, then set the config value
-    app.config.__dict__[key] = value
-    return app
+def _config_provided_by_user(app, key):
+    """Check if the user has manually provided the config."""
+    return any(key in ii for ii in [app.config.overrides, app.config._raw_config])
 
 
 def update_config(app):
@@ -89,7 +79,8 @@ def update_config(app):
         )
 
     # Set the anchor link default to be # if the user hasn't provided their own
-    _set_config_if_not_provided_by_user(app, "html_permalinks_icon", "#")
+    if not _config_provided_by_user(app, "html_permalinks_icon"):
+        app.config.html_permalinks_icon = "#"
 
     # Raise a warning for a deprecated theme switcher config
     # TODO: deprecation; remove after 0.13 release
@@ -175,8 +166,10 @@ def update_config(app):
             app.add_js_file(None, body=gid_script)
 
     # Update ABlog configuration default if present
-    if "ablog" in app.config.extensions:
-        _set_config_if_not_provided_by_user(app, "fontawesome_included", True)
+    if "ablog" in app.config.extensions and not _config_provided_by_user(
+        app, "fontawesome_included"
+    ):
+        app.config.fontawesome_included = True
 
     # Prepare the logo config dictionary
     theme_logo = theme_options.get("logo")
