6f938baa9d369d6c3bc37b2d76a7b92f7c507c5e
gabalafou <gabriel@fouasnon.com>
2023-03-27 09:20:01 -0500

    Update tests/utils.py
    Co-authored-by: Daniel McCloy <dan@mccloy.info>

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,7 @@
 # This code taken from:
 # https://github.com/mozilla-services/axe-selenium-python/blob/3cfbdd67c9b40ab03f37b3ba2521f77c2071827b/axe_selenium_python/axe.py
 # Typical usage:
-# assert(len(results["violations"])) = 0, pretty_axe_results(results)
+# assert(len(results["violations"])) == 0, pretty_axe_results(results)
 def pretty_axe_results(results: dict) -> str:
     """
     Return readable report of accessibility violations found.
