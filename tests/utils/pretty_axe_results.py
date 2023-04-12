f1c8007218278ebd4f1b6194a7425d1a7601a47d
gabalafou <gabriel@fouasnon.com>
2023-04-12 10:00:18 -0500

    fix pretty_axe_results

--- tests/utils/pretty_axe_results.py
+++ tests/utils/pretty_axe_results.py
@@ -1,18 +1,23 @@
+"""Readable report of accessibility violations."""
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
+
 # This code taken from:
 # https://github.com/mozilla-services/axe-selenium-python/blob/3cfbdd67c9b40ab03f37b3ba2521f77c2071827b/axe_selenium_python/axe.py
 # Typical usage:
 # assert(len(results["violations"])) == 0, pretty_axe_results(results)
+
+
 def pretty_axe_results(results: dict) -> str:
-    """
-    Return readable report of accessibility violations found.
+    """Create readable string that can be printed to console from the Axe-core results object.
+
     :param results: The object promised by `axe.run()`.
     :type results: dict
     :return report: Readable report of violations.
-    :rtype: string
+    :rtype: string.
+
     """
     violations = results["violations"]
     string = ""
@@ -32,10 +37,10 @@ def pretty_axe_results(results: dict) -> str:
                 string += f"\n\t {i} Target: {target}"
                 i += 1
             for item in node["all"]:
-                string += f"\n\t\t {item["message"]}"
+                string += f"\n\t\t {item['message']}"
             for item in node["any"]:
-                string += f"\n\t\t {item["message"]}"
+                string += f"\n\t\t {item['message']}"
             for item in node["none"]:
-                string += f"\n\t\t {item["message"]}"
+                string += f"\n\t\t {item['message']}"
         string += "\n\n\n"
     return string
