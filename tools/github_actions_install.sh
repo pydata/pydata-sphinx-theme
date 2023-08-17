dcd82adfe8d1617fe4f688afb825fe4f776e1ceb
Eric Larson <larson.eric.d@gmail.com>
2023-08-17 16:11:51 -0400

    MAINT: Test old and new Sphinx versions (#1413)
    * MAINT: Test old and new Sphinx versions
    
    * FIX: Comment
    
    * FIX: git plus
    
    * FIX: Force
    
    * FIX: Bump
    
    * FIX: old
    
    * FIX: And concurrency
    
    * FIX: Try again
    
    * FIX: Allow
    
    * FIX: Try one moer
    
    * FIX: Try again again
    
    * FIX: Cant spell
    
    * FIX: Culprit
    
    * FIX: Pathy
    
    * TST: Ping

---
+++ tools/github_actions_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# First arg ($1) is the extras_require to install, optional second arg
+# ($2) is an extra dependency (currently just nox on one run)
+set -eo pipefail
+export PYTHONUTF8=1
+if [[ "$SPHINX_VERSION" == "" ]]; then
+    SPHINX_INSTALL=""
+elif [[ "$SPHINX_VERSION" == "dev" ]]; then
+    SPHINX_INSTALL="git+https://github.com/sphinx-doc/sphinx"
+    if [[ "$1" == "doc" ]]; then
+        # Until they release a new version that undoes the max sphinx pin...
+        DEP_EXTRA="git+https://github.com/executablebooks/MyST-NB git+https://github.com/larsoner/sphinx-sitemap.git@path"
+    fi
+elif [[ "$SPHINX_VERSION" == "old" ]]; then
+    SPHINX_INSTALL="sphinx==5.0"
+else  # not used currently but easy enough
+    SPHINX_INSTALL="sphinx==$SPHINX_VERSION"
+fi
+set -x  # print commands
+python -m pip install --upgrade pip wheel setuptools
+python -m pip install -e .["$1"] ${SPHINX_INSTALL} $2 $DEP_EXTRA
