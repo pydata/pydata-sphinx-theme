039e2b47f95b8fdccb84aeb831abf9b544f4fe2d
Eric Larson <larson.eric.d@gmail.com>
2023-08-17 11:47:35 -0400

    FIX: Try one moer

--- tools/github_actions_install.sh
+++ tools/github_actions_install.sh
@@ -8,6 +8,9 @@ if [[ "$SPHINX_VERSION" == "" ]]; then
     SPHINX_INSTALL=""
 elif [[ "$SPHINX_VERSION" == "dev" ]]; then
     SPHINX_INSTALL="git+https://github.com/sphinx-doc/sphinx"
+    if [[ "$1" == "doc" ]]; then
+        DEP_EXTRA="jupyterlite-sphinx==0.9.1"
+    fi
 elif [[ "$SPHINX_VERSION" == "old" ]]; then
     # ablog requires 5.0 so we have to triage based on install type
     if [[ "$1" == "doc" ]]; then
@@ -20,4 +23,4 @@ else  # not used currently but easy enough
 fi
 set -x  # print commands
 python -m pip install --upgrade pip wheel setuptools
-python -m pip install -e .["$1"] ${SPHINX_INSTALL} $2
+python -m pip install -e .["$1"] ${SPHINX_INSTALL} $2 $DEP_EXTRA
