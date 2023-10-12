badecee75dcb49da601436f65781426e9a6e52d3
Adam Porter <adam@alphapapa.net>
2023-10-12 12:26:40 -0500

    Fix: (webpack.config.js) css-loader API change
    The build was broken in
    <https://github.com/pydata/pydata-sphinx-theme/commit/185a37aa36820f77bffa4c87a772092e9e7cc380>/<https://github.com/pydata/pydata-sphinx-theme/pull/1494>.
    
    This change fixes the build, and it seems to be in accordance with the
    current API as described at <https://github.com/webpack-contrib/css-loader/blob/c6f36cf91ac61743a70e81cfb077faa0f8730ebe/README.md#boolean>.
    
    Closes <https://github.com/pydata/pydata-sphinx-theme/issues/1507>.

--- webpack.config.js
+++ webpack.config.js
@@ -159,7 +159,9 @@ module.exports = {
       test: /\.scss$/,
       use: [
         {loader: MiniCssExtractPlugin.loader},
-        {loader: "css-loader?-url"}, //url()-inlining turned off
+        {loader: "css-loader"},
+        {loader: "css-loader",
+	 options: { url: false }},
         {loader: "sass-loader",},
       ],
     }],
