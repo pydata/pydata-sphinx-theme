368008056f438a3ce19c8d184d73bf07eca6a614
Daniel McCloy <dan@mccloy.info>
2023-10-16 14:31:54 -0500

    convert "stable" to actual version number

--- docs/conf.py
+++ docs/conf.py
@@ -99,19 +99,21 @@ json_url = "https://pydata-sphinx-theme.readthedocs.io/en/latest/_static/switche
 
 # Define the version we use for matching in the version switcher.
 version_match = os.environ.get("READTHEDOCS_VERSION")
+release = pydata_sphinx_theme.__version__
 # If READTHEDOCS_VERSION doesn't exist, we're not on RTD
 # If it is an integer, we're in a PR build and the version isn't correct.
 # If it's "latest" → change to "dev" (that's what we want the switcher to call it)
 if not version_match or version_match.isdigit() or version_match == "latest":
     # For local development, infer the version to match from the package.
-    release = pydata_sphinx_theme.__version__
     if "dev" in release or "rc" in release:
         version_match = "dev"
         # We want to keep the relative reference if we are in dev mode
         # but we want the whole url if we are effectively in a released version
         json_url = "_static/switcher.json"
     else:
-        version_match = "v" + release
+        version_match = f"v{release}"
+elif version_match == "stable":
+    version_match = f"v{release}"
 
 html_theme_options = {
     "external_links": [
