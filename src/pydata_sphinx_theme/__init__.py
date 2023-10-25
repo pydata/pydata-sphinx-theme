9fc085449fbcd866c15483b765ca79071af6eaad
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2023-10-25 07:10:56 +0000

    back to dev

--- src/pydata_sphinx_theme/__init__.py
+++ src/pydata_sphinx_theme/__init__.py
@@ -14,7 +14,7 @@ from sphinx.util import logging
 
 from . import edit_this_page, logo, pygment, short_link, toctree, translator, utils
 
-__version__ = "0.14.2"
+__version__ = "0.14.3dev0"
 
 logger = logging.getLogger(__name__)
 
