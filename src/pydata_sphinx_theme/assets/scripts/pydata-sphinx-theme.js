5e535892bf73e741ffaddc3a49a2d3d44d50d3a1
gabalafou <gabriel@fouasnon.com>
2023-12-07 23:02:21 +0200

    Fix landmarks (#1454)
    * add header
    
    * fix nav landmarks
    
    * only one main
    
    * label indices nav
    
    * few landmarks
    
    * unique id todos
    
    * fix test fixtures
    
    * fix translations
    
    * translations?
    
    * make version warning a landmark
    
    * revert translation changes
    
    * revert changes to noxfile.py
    
    * update tests to reflect translation reversal
    
    * add TODO comment to make string translatable

--- src/pydata_sphinx_theme/assets/scripts/pydata-sphinx-theme.js
+++ src/pydata_sphinx_theme/assets/scripts/pydata-sphinx-theme.js
@@ -468,7 +468,9 @@ function showVersionWarningBanner(data) {
     return;
   }
   // now construct the warning banner
-  var outer = document.createElement("div");
+  var outer = document.createElement("aside");
+  // TODO: add to translatable strings
+  outer.setAttribute("aria-label", "Version warning");
   const middle = document.createElement("div");
   const inner = document.createElement("div");
   const bold = document.createElement("strong");
@@ -504,7 +506,8 @@ function showVersionWarningBanner(data) {
   inner.appendChild(bold);
   inner.appendChild(document.createTextNode("."));
   inner.appendChild(button);
-  document.body.prepend(outer);
+  const skipLink = document.getElementById("pst-skip-link");
+  skipLink.after(outer);
 }
 
 /*******************************************************************************
