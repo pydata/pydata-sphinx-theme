1f81622a10ec7c32a2f7b7f2af5a58d3b76a0efc
Daniel McCloy <dan@mccloy.info>
2023-12-11 10:51:05 -0600

    Revert "frozen data class"
    This reverts commit 3b0c73e93d1f1b52f45b4918015c66fe5142c294.

--- tests/test_a11y.py
+++ tests/test_a11y.py
@@ -1,7 +1,6 @@
 """Using Axe-core, scan the Kitchen Sink pages for accessibility violations."""
 
 import time
-from dataclasses import dataclass
 from http.client import HTTPConnection
 from pathlib import Path
 from subprocess import PIPE, Popen
@@ -71,30 +70,20 @@ def url_base():
         process.wait()
 
 
-@dataclass(frozen=True)
-class AxeViolationFingerprint:
-    """Fingerprint for single Axe violation."""
-
-    id: str
-    help: str
-    helpUrl: str
-    targets: tuple
-
-
-def fingerprint(accessibility_page_scan_violations):
+def fingerprint_violations(accessibility_page_scan_violations):
     """Create a fingerprint of the Axe violations array.
 
     https://playwright.dev/docs/accessibility-testing#using-snapshots-to-allow-specific-known-issues
     """
-    return tuple(
-        AxeViolationFingerprint(
-            violation["id"],
-            violation["help"],
-            violation["helpUrl"],
-            tuple(node["target"] for node in violation["nodes"]),
-        )
+    return [
+        {
+            "id": violation["id"],
+            "help": violation["help"],
+            "helpUrl": violation["helpUrl"],
+            "targets": [node["target"] for node in violation["nodes"]],
+        }
         for violation in accessibility_page_scan_violations
-    )
+    ]
 
 
 @pytest.mark.a11y
@@ -182,7 +171,7 @@ def test_axe_core(
     results = page.evaluate("axe.run()" if selector == "" else f"axe.run('{selector}')")
 
     # Check found violations against known violations that we do not plan to fix
-    data_regression.check(fingerprint(results["violations"]))
+    data_regression.check(fingerprint_violations(results["violations"]))
 
 
 def test_version_switcher_highlighting(page: Page, url_base: str) -> None:
