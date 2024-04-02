63d84621336ebcdb6777db255c536c9aad3378f3
Daniel McCloy <dan@mccloy.info>
2024-04-02 09:28:12 -0500

    try without whitespace

--- tools/github_actions_install.sh
+++ tools/github_actions_install.sh
@@ -7,7 +7,7 @@ export PYTHONUTF8=1
 if [[ "$SPHINX_VERSION" == "" ]]; then
     SPHINX_INSTALL=""
 elif [[ "$SPHINX_VERSION" == "dev" ]]; then
-    SPHINX_INSTALL='"sphinx[test] @ https://codeload.github.com/sphinx-doc/sphinx/zip/refs/heads/master"'
+    SPHINX_INSTALL="sphinx[test]@https://codeload.github.com/sphinx-doc/sphinx/zip/refs/heads/master"
 elif [[ "$SPHINX_VERSION" == "old" ]]; then
     SPHINX_INSTALL="sphinx[test]==6.1.0"
 else  # not used currently but easy enough
@@ -15,4 +15,4 @@ else  # not used currently but easy enough
 fi
 set -x  # print commands
 python -m pip install --upgrade pip wheel setuptools
-python -m pip install -e .["$1"] "${SPHINX_INSTALL}" $2
+python -m pip install -e .["$1"] ${SPHINX_INSTALL} $2
