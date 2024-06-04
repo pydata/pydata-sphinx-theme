96de332063925280db43aa2d9224d658d9512ac0
Paul Ivanov <pi@berkeley.edu>
2024-06-04 15:00:38 -0700

    initial breadcrumbs standalone test
    Co-authored-by: Madicken Munk <madicken.munk@gmail.com>

---
+++ tests/test_breadcrumbs.py
@@ -0,0 +1,88 @@
+"""Using Axe-core, scan the Kitchen Sink pages for accessibility violations."""
+
+import re
+import time
+from http.client import HTTPConnection
+from pathlib import Path
+from subprocess import PIPE, Popen
+from urllib.parse import urljoin
+
+import pytest
+
+# Using importorskip to ensure these tests are only loaded if Playwright is installed.
+playwright = pytest.importorskip("playwright")
+from playwright.sync_api import Page, expect  # noqa: E402
+
+# Important note: automated accessibility scans can only find a fraction of
+# potential accessibility issues.
+#
+# This test file scans pages from the Kitchen Sink examples with a JavaScript
+# library called Axe-core, which checks the page for accessibility violations,
+# such as places on the page with poor color contrast that would be hard for
+# people with low vision to see.
+#
+# Just because a page passes the scan with no accessibility violations does
+# *not* mean that it will be generally usable by a broad range of disabled
+# people. It just means that page is free of common testable accessibility
+# pitfalls.
+
+path_repo = Path(__file__).parent.parent
+path_docs_build = path_repo / "docs" / "_build" / "html"
+
+
+@pytest.fixture(scope="module")
+def url_base(path_repo=path_repo, path_docs_build=path_docs_build):
+    """Start local server on built docs and return the localhost URL as the base URL."""
+    # Use a port that is not commonly used during development or else you will
+    # force the developer to stop running their dev server in order to run the
+    # tests.
+    port = "8213"
+    host = "localhost"
+    url = f"http://{host}:{port}"
+
+    # Try starting the server
+    process = Popen(
+        ["python", "-m", "http.server", port, "--directory", path_docs_build],
+        stdout=PIPE,
+    )
+
+    # Try connecting to the server
+    retries = 5
+    while retries > 0:
+        conn = HTTPConnection(host, port)
+        try:
+            conn.request("HEAD", "/")
+            response = conn.getresponse()
+            if response is not None:
+                yield url
+                break
+        except ConnectionRefusedError:
+            time.sleep(1)
+            retries -= 1
+
+    # If the code above never yields a URL, then we were never able to connect
+    # to the server and retries == 0.
+    if not retries:
+        raise RuntimeError("Failed to start http server in 5 seconds")
+    else:
+        # Otherwise the server started and this fixture is done now and we clean
+        # up by stopping the server.
+        process.terminate()
+        process.wait()
+
+
+def test_breadcrumb_expansion(page: Page, url_base: str) -> None:
+    page.set_viewport_size({"width": 1440, "height": 720})
+    page.goto(urljoin(url_base, "community/topics/config.html"))
+    expect(page.get_by_label("Breadcrumb").get_by_role("list")).to_contain_text("Update Sphinx configuration during the build")
+    el = page.get_by_text("Update Sphinx configuration during the build").nth(1)
+    assert el.evaluate("e => e.clientWidth === e.scrollWidth", el) == True
+    expect(el).to_have_css("overflow-x", "hidden")
+    expect(el).to_have_css("text-overflow", "ellipsis")
+    page.set_viewport_size({"width": 20, "height": 720})
+
+    # There is no good way to check if text-overflow has been applied other
+    # than to directly compare the rendered clientWidth to the scrollWidth
+    # required to display the text.
+    assert el.evaluate("e => e.clientWidth < e.scrollWidth", el) == True
+
