90820b0696e7c72814ed41cd2757984e6c72e87d
Tania Allard <taniar.allard@gmail.com>
2024-07-19 15:57:56 +0100

    Remove unecessary copy plugins

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@
 
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
@@ -30,12 +29,9 @@ exec(`pybabel compile -d ${localePath} -D sphinx`);
  * Paths for various assets (sources and destinations)
  */
 
-const vendorVersions = { fontAwesome: require("@fortawesome/fontawesome-free/package.json").version };
-
 const scriptPath = resolve(__dirname, "src/pydata_sphinx_theme/assets/scripts");
 const staticPath = resolve(__dirname, "src/pydata_sphinx_theme/theme/pydata_sphinx_theme/static");
 const vendorPath = resolve(staticPath, "vendor");
-const faPath = { fontAwesome: resolve(vendorPath, "fontawesome", vendorVersions.fontAwesome) };
 
 /*******************************************************************************
  * functions to load the assets in the html head
@@ -46,7 +42,6 @@ const faPath = { fontAwesome: resolve(vendorPath, "fontawesome", vendorVersions.
 function stylesheet(css) { return `<link href="{{ pathto('_static/${css}', 1) }}?digest=${this.hash}" rel="stylesheet" />`; }
 function preload(js) { return `<link rel="preload" as="script" href="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}" />`; }
 function script(js) { return `<script src="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}"></script>`; }
-function font(woff2) { return `<link rel="preload" as="font" type="font/woff2" crossorigin href="{{ pathto('_static/${woff2}', 1) }}" />`; }
 
 /*******************************************************************************
  * the assets to load in the macro
@@ -59,17 +54,6 @@ const theme_stylesheets = [
 const theme_scripts = [
   "scripts/bootstrap.js",
   "scripts/pydata-sphinx-theme.js",
-];
-const fa_stylesheets = [
-  `vendor/fontawesome/${vendorVersions.fontAwesome}/css/all.min.css`,
-];
-const fa_scripts = [
-  `vendor/fontawesome/${vendorVersions.fontAwesome}/js/all.min.js`,
-];
-const fa_fonts = [
-  `vendor/fontawesome/${vendorVersions.fontAwesome}/webfonts/fa-solid-900.woff2`,
-  `vendor/fontawesome/${vendorVersions.fontAwesome}/webfonts/fa-brands-400.woff2`,
-  `vendor/fontawesome/${vendorVersions.fontAwesome}/webfonts/fa-regular-400.woff2`,
 ];
 
 /*******************************************************************************
@@ -85,11 +69,6 @@ function macroTemplate({ compilation }) {
       AUTO-GENERATED from webpack.config.js, do **NOT** edit by hand.
       These are re-used in layout.html
     -->
-    {# Load FontAwesome icons #}
-    {% macro head_pre_icons() %}
-      ${fa_stylesheets.map(stylesheet.bind(compilation)).join("\n")}
-      ${fa_fonts.map(font).join("\n")}
-    {% endmacro %}
 
     {% macro head_pre_assets() %}
       <!-- Loaded before other Sphinx assets -->
@@ -99,7 +78,6 @@ function macroTemplate({ compilation }) {
     {% macro head_js_preload() %}
       <!-- Pre-loaded scripts that we'll load fully later -->
       ${theme_scripts.map(preload.bind(compilation)).join("\n")}
-      ${fa_scripts.map(script.bind(compilation)).join("\n")}
     {% endmacro %}
 
     {% macro body_post() %}
@@ -121,31 +99,6 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
   templateContent: macroTemplate,
 });
 
-const copyPlugin = new CopyPlugin({ // fontawesome
-  patterns: [
-    {
-      context: "./node_modules/@fortawesome/fontawesome-free",
-      from: "LICENSE.txt",
-      to: resolve(faPath.fontAwesome, "LICENSE.txt"),
-    },
-    {
-      context: "./node_modules/@fortawesome/fontawesome-free/css",
-      from: "all.min.css",
-      to: resolve(faPath.fontAwesome, "css"),
-    },
-    {
-      context: "./node_modules/@fortawesome/fontawesome-free/js",
-      from: "all.min.js",
-      to: resolve(faPath.fontAwesome, "js"),
-    },
-    {
-      context: "./node_modules/@fortawesome/fontawesome-free",
-      from: "webfonts",
-      to: resolve(faPath.fontAwesome, "webfonts"),
-    },
-  ]
-});
-
 module.exports = {
   mode: "production",
   devtool: "source-map",
@@ -153,14 +106,18 @@ module.exports = {
     "pydata-sphinx-theme": resolve(scriptPath, "pydata-sphinx-theme.js"),
     "bootstrap": resolve(scriptPath, "bootstrap.js"),
   },
-  // output: { filename: "scripts/[name].js", path: staticPath },
-  // debugging
   output: {
     filename: "scripts/[name].js",
-    path: resolve(__dirname, 'dist'),
-    // clean build directory before each build
-    clean: true,
+    path: staticPath,
+    // clean: true,
   },
+  // debugging
+  // output: {
+  //   filename: "scripts/[name].js",
+  //   path: resolve(__dirname, 'dist'),
+  //   // clean build directory before each build
+  //   clean: true,
+  // },
   optimization: {
     minimizer: [
       '...',
@@ -198,13 +155,12 @@ module.exports = {
       test: /\.(woff|woff2|eot|ttf|otf)$/i,
       type: 'asset/resource',
       generator: {
-        filename: 'vendor/webfonts/[name][ext]'
+        filename: 'vendor/fontawesome/webfonts/[name][ext]'
       }
     },],
   },
   plugins: [
     htmlWebpackPlugin,
-    copyPlugin,
     new MiniCssExtractPlugin({
       filename: "styles/[name].css"
     })],
