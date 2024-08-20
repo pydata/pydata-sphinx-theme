c133db0a63e435a58e73c3024ee5b9f5ecad5fff
M Bussonnier <bussonniermatthias@gmail.com>
2024-08-20 09:57:39 +0200

    A11Y: Add title attribute to theme switcher
    This will add a `<title>` attribute to the SVG generated by font
    awesome, which will in turn update the Accessibility tree, which will
    now be
    
      - button "light/dark" focusable: true focused: bool
         - image: $title-text
    
    Should fix Quansight-Labs/czi-scientific-python-mgmt#76

--- tests/test_build.py
+++ tests/test_build.py
@@ -996,7 +996,15 @@ def test_translations(sphinx_build_factory) -> None:
     assert "Modifier sur GitHub" in str(sidebar_secondary)
 
     header = index.select(".bd-header")[0]
-    assert "clair/sombre" in str(header)
+    ## TODO: update once translataion up to date
+    # assert "clair/sombre" in str(header)
+    # Text of theme switcher button have been changed,
+    # "light/dark" has been updated to "Color mode" and does not have a translation yet.
+    if "Color mode" not in str(header):
+        pytest.xfail(
+            "Please update test_build.py::test_translations now that new translation are available."
+        )
+    # End TODO
 
     footer = index.select(".bd-footer")[0]
     assert "Copyright" in str(footer)
