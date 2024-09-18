68b264957a3d7bb8603e2448ba1afea720dfa923
Tania Allard <taniar.allard@gmail.com>
2024-09-18 15:34:19 +0100

    Add FA.JS to head

--- src/pydata_sphinx_theme/assets/scripts/fontawesome.js
+++ src/pydata_sphinx_theme/assets/scripts/fontawesome.js
@@ -1,5 +1,5 @@
 // TODO: @trallard might end up moving this to the main JS file
 // Define the custom behavior of the page
-import { documentReady } from "./mixin";
 
+import "@fortawesome/fontawesome-free/js/fontawesome.min.js";
 import "@fortawesome/fontawesome-free/js/all.min.js";
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,6 @@ exec(`pybabel compile -d ${localePath} -D sphinx`);
 
 const scriptPath = resolve(__dirname, "src/pydata_sphinx_theme/assets/scripts");
 const staticPath = resolve(__dirname, "src/pydata_sphinx_theme/theme/pydata_sphinx_theme/static");
-const vendorPath = resolve(staticPath, "vendor");
 
 /*******************************************************************************
  * functions to load the assets in the html head
@@ -42,6 +41,8 @@ const vendorPath = resolve(staticPath, "vendor");
 function stylesheet(css) { return `<link href="{{ pathto('_static/${css}', 1) }}?digest=${this.hash}" rel="stylesheet" />`; }
 function preload(js) { return `<link rel="preload" as="script" href="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}" />`; }
 function script(js) { return `<script defer src="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}"></script>`; }
+// Adding FA without preloading
+function fascript(js) { return `<script src="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}"></script>`; }
 
 /*******************************************************************************
  * the assets to load in the macro
@@ -78,13 +79,14 @@ function macroTemplate({ compilation }) {
     {% endmacro %}
 
     {% macro head_js_preload() %}
-      <!-- Pre-loaded scripts that we'll load fully later -->
+    <!-- So that users can add custom icons -->
+    ${fa_scripts.map(fascript.bind(compilation)).join("\n")}
+    <!-- Pre-loaded scripts that we'll load fully later -->
       ${theme_scripts.map(preload.bind(compilation)).join("\n")}
     {% endmacro %}
 
     {% macro body_post() %}
       <!-- Scripts loaded after <body> so the DOM is not blocked -->
-      ${fa_scripts.map(script.bind(compilation)).join("\n")}
       ${theme_scripts.map(script.bind(compilation)).join("\n")}
     {% endmacro %}
   `);
