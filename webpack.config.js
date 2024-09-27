e7e9749c467ae78c26c4d055fd0fda0656382338
Tania Allard <taniar.allard@gmail.com>
2024-09-27 16:28:07 +0100

    DEV - Add Fontawesome scripts to build assets (#1982)
    I seem to have forgotten to add the FA scripts to the `pyproject.toml`
    so I am adding it here (which should fix the FA rendering issue in the
    dev version of the docs).
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,6 @@ const { Compilation } = require("webpack");
 
 const scriptPath = resolve(__dirname, "src/pydata_sphinx_theme/assets/scripts");
 const staticPath = resolve(__dirname, "src/pydata_sphinx_theme/theme/pydata_sphinx_theme/static");
-const vendorPath = resolve(staticPath, "vendor");
 
 /*******************************************************************************
  * functions to load the assets in the html head
@@ -32,8 +31,10 @@ const vendorPath = resolve(staticPath, "vendor");
  */
 
 function stylesheet(css) { return `<link href="{{ pathto('_static/${css}', 1) }}?digest=${this.hash}" rel="stylesheet" />`; }
-function preload(js) { return `<link rel="preload" as="script" href="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}" />`; }
-function script(js) { return `<script defer src="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}"></script>`; }
+function preloadScript(js) { return `<link rel="preload" as="script" href="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}" />`; }
+function deferScript(js) { return `<script defer src="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}"></script>`; }
+// Adding FA without preloading
+function script(js) { return `<script src="{{ pathto('_static/${js}', 1) }}?digest=${this.hash}"></script>`; }
 
 /*******************************************************************************
  * the assets to load in the macro
@@ -70,14 +71,15 @@ function macroTemplate({ compilation }) {
     {% endmacro %}
 
     {% macro head_js_preload() %}
+      <!-- So that users can add custom icons -->
+      ${fa_scripts.map(script.bind(compilation)).join("\n")}
       <!-- Pre-loaded scripts that we'll load fully later -->
-      ${theme_scripts.map(preload.bind(compilation)).join("\n")}
+      ${theme_scripts.map(preloadScript.bind(compilation)).join("\n")}
     {% endmacro %}
 
     {% macro body_post() %}
       <!-- Scripts loaded after <body> so the DOM is not blocked -->
-      ${fa_scripts.map(script.bind(compilation)).join("\n")}
-      ${theme_scripts.map(script.bind(compilation)).join("\n")}
+      ${theme_scripts.map(deferScript.bind(compilation)).join("\n")}
     {% endmacro %}
   `);
 }
