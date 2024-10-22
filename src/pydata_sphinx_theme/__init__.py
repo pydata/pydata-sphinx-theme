fad36474675c7ca5b322f7f4e2495feaca9d7924
Tania Allard <taniar.allard@gmail.com>
2024-10-22 15:12:14 +0100

    bump: 0.16.0rc0 -> 0.16.0

--- src/pydata_sphinx_theme/__init__.py
+++ src/pydata_sphinx_theme/__init__.py
@@ -14,7 +14,7 @@ from sphinx.errors import ExtensionError
 
 from . import edit_this_page, logo, pygments, short_link, toctree, translator, utils
 
-__version__ = "0.16.0rc0"
+__version__ = "0.16.0"
 
 
 def update_config(app):
