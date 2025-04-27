b032efc970abd1f5251906ec4941ec399ae84d35
Tetsuo Koyama <tkoyama010@gmail.com>
2025-04-27 20:40:16 +0900

    Fix pre-commit error

--- tests/test_language_version_switcher.py
+++ tests/test_language_version_switcher.py
@@ -1,9 +1,14 @@
+"""Tests for the language and version switcher functionality."""
+
 import pytest
+
 from bs4 import BeautifulSoup
+
 
 @pytest.fixture
 def sample_html():
-    return '''
+    """Provide sample HTML for testing."""
+    return """
     <div class="navbar-switcher">
       <select id="language-switcher">
         <option value="en" selected>English</option>
@@ -14,22 +19,26 @@ def sample_html():
         <option value="https://example.com/en/stable/">Stable</option>
       </select>
     </div>
-    '''
+    """
+
 
 def test_language_switcher(sample_html):
-    soup = BeautifulSoup(sample_html, 'html.parser')
+    """Test the language switcher dropdown."""
+    soup = BeautifulSoup(sample_html, "html.parser")
     language_switcher = soup.find(id="language-switcher")
     assert language_switcher is not None
-    options = language_switcher.find_all('option')
+    options = language_switcher.find_all("option")
     assert len(options) == 2
-    assert options[0]['value'] == 'en'
-    assert options[1]['value'] == 'ja'
+    assert options[0]["value"] == "en"
+    assert options[1]["value"] == "ja"
+
 
 def test_version_switcher(sample_html):
-    soup = BeautifulSoup(sample_html, 'html.parser')
+    """Test the version switcher dropdown."""
+    soup = BeautifulSoup(sample_html, "html.parser")
     version_switcher = soup.find(id="version-switcher")
     assert version_switcher is not None
-    options = version_switcher.find_all('option')
+    options = version_switcher.find_all("option")
     assert len(options) == 2
-    assert options[0]['value'] == 'https://example.com/en/latest/'
-    assert options[1]['value'] == 'https://example.com/en/stable/'
+    assert options[0]["value"] == "https://example.com/en/latest/"
+    assert options[1]["value"] == "https://example.com/en/stable/"
