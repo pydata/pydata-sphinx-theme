df8a6804de5ad15c527256cc38a26a324335545e
Tetsuo Koyama <tkoyama010@gmail.com>
2025-04-27 20:33:40 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -150,7 +150,6 @@ if not version_match or version_match.isdigit() or version_match == "latest":
 elif version_match == "stable":
     version_match = f"v{release}"
 
-# Update the switcher configuration to include language_match
 html_theme_options = {
     "external_links": [
         {
