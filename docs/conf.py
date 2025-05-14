43b427707d848b8cff25ac35429b71146c682c98
gabalafou <gabriel@fouasnon.com>
2025-05-14 10:54:56 +0200

    use relative link

--- docs/conf.py
+++ docs/conf.py
@@ -259,6 +259,7 @@ html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 html_js_files = [
     ("custom-icons.js", {"defer": "defer"}),
+    ("pydata-icon.js", {"defer": "defer"}),
 ]
 todo_include_todos = True
 
