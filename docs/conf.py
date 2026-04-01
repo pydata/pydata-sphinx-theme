9975ba4c9e706651883039bc61fc2d80078dec38
Yann Pellegrini <mail@yann-p.fr>
2026-04-01 15:19:13 +0200

    Increase linkcheck timeout
    - timeouts have been the reason for the latest failures
    - time is not an issue anymore because it runs daily and does not block
    merge requests #2329`

--- docs/conf.py
+++ docs/conf.py
@@ -413,5 +413,5 @@ linkcheck_allowed_redirects = {
 
 # we have had issues with linkcheck timing and retries on www.gnu.org
 linkcheck_retries = 1
-linkcheck_timeout = 5
+linkcheck_timeout = 30
 linkcheck_report_timeouts_as_broken = True
